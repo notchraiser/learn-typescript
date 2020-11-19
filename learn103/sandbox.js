b55a62354f2d5642ddc0dab9c2a859388002e219
DravitKumar <dravitkumar@vm.mshome.net>
2020-11-19 20:38:49 +0530

    learn103

---
+++ learn103/sandbox.js
@@ -0,0 +1,13 @@
+var character = 'Bijli';
+var age = 10;
+var isMale = true;
+//character = 56;
+character = 'jio';
+//age = 'moha';
+age = 90;
+//isMale = 'hjh';
+isMale = false;
+var area = function (diameter) {
+    return diameter * Math.PI;
+};
+console.log(area(10));
---
+++ learn103/sandbox.ts
@@ -0,0 +1,18 @@
+let character:string = 'Bijli';
+let age:number = 10;
+let isMale:boolean = true;
+
+
+//character = 56;
+character = 'jio';
+//age = 'moha';
+age = 90;
+
+//isMale = 'hjh';
+isMale = false;
+
+const area = (diameter: number):number => {
+    return diameter * Math.PI;
+}
+
+console.log(area(10));
